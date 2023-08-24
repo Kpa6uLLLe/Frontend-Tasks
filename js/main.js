4ca4bd189d37a070a06f6f7026d88b8818e9c4ba
Kpa6uLLLe <103505023+Kpa6uLLLe@users.noreply.github.com>
2023-08-24 14:56:43 +0600

    2
    task2 done

--- js/main.js
+++ js/main.js
@@ -10,3 +10,28 @@ document.querySelectorAll('.accordion_button').forEach((item) => {
    }
  });
 });
+
+document.querySelectorAll('.item_category').forEach((item) => {
+item.innerHTML = "Кресла театральные";
+});
+document.querySelectorAll('.item_name').forEach((item) => {
+item.innerHTML = "Театральное кресло Прайм";
+});
+document.querySelectorAll('.item_article').forEach((item) => {
+item.innerHTML = 'Артикул: <div class="article_numbers">789-2341</div>';
+});
+document.querySelectorAll('.item_price').forEach((item) => {
+item.innerHTML = 'От: <div class="price_numbers">6762</div> Р';
+});
+document.querySelectorAll('.item_mod_discount').forEach((item) => {
+item.innerHTML = '<div class="discount_icon"></div> акция';
+});
+document.querySelectorAll('.item_mod_hot').forEach((item) => {
+item.innerHTML = '<div class="hot_icon"></div> хит продаж';
+});
+document.querySelectorAll('.item_mod_new').forEach((item) => {
+item.innerHTML = '<div class="new_icon"></div> новинка';
+});
+document.querySelectorAll('.item_mod_instock').forEach((item) => {
+item.innerHTML = '<div class="instock_icon"></div> В наличии';
+});
